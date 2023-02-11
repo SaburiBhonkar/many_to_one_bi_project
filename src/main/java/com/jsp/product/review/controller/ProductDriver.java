dac6a5d3722b4e90b7de8342aea89694b818a049
SaburiBhonkar <bhonkarsaburi67@gmail.com>
2023-02-11 10:09:54 +0530

    many_to_one_bi_project

---
+++ src/main/java/com/jsp/product/review/controller/ProductDriver.java
@@ -0,0 +1,58 @@
+package com.jsp.product.review.controller;
+
+import com.jsp.product.review.dto.Product;
+import com.jsp.product.review.service.ProductService;
+
+public class ProductDriver {
+
+	public static void main(String[] args) {
+
+// Insert A Record
+
+//		Product product = new Product();
+//		product.setName("Saburi");
+//		product.setBrand("Max");
+//		product.setType("TOP");
+//		product.setCost(345);
+//
+//		ProductService productService = new ProductService();
+//		productService.saveProduct(product);
+
+// Get By Id
+
+//		ProductService productService = new ProductService();
+//		Product p = productService.getByIdProduct(1);
+//
+//		System.out.println(p.getId());
+//		System.out.println(p.getName());
+//		System.out.println(p.getBrand());
+//		System.out.println(p.getType());
+//		System.out.println(p.getCost());
+
+// Delete A Record
+
+//		Product product = new Product();
+//		product.setId(1);
+//
+//		ProductService productService = new ProductService();
+//		productService.deleteProduct(product);
+
+// Update A Record
+
+//		Product product = new Product();
+//		product.setId(1);
+//		product.setName("Sou");
+//		product.setBrand("Fila");
+//		product.setType("Shooe");
+//		product.setCost(1500);
+//
+//		ProductService productService = new ProductService();
+//		productService.updateProduct(product);
+
+// Get All details
+
+//		ProductService productService = new ProductService();
+//		productService.getAllDetails();
+
+	}
+}
